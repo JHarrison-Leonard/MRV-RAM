0c341c2a67e622b631579a2ee08cc335d886e4d8
JHLeonard <JHarrison.Leonard@yahoo.com>
2021-03-09 13:27:53 -0600

    Added servo definiitions

--- modules/RAM/AVR/main.h
+++ modules/RAM/AVR/main.h
@@ -7,7 +7,11 @@
 #define MODULE_PROBE_STRING "Module?"
 #define MODULE_NAME "RAM"
 
+// Command characters
 #define SHOULDER_CHAR 'S'
+#define ELBOW_CHAR 'E'
+#define WRIST_CHAR 'W'
+#define CLAW_CHAR 'C'
 
 #define SERVO_FREQUENCY 50 //Hz
 
@@ -15,6 +19,21 @@
 #define SHOULDER_MIN 800
 #define SHOULDER_MAX 2200
 #define SHOULDER_DEFAULT 1500
+
+// Elbow pulse width definitions in microseconds (us)
+#define ELBOW_MIN 500
+#define ELBOW_MAX 2500
+#define ELBOW_DEFAULT 1500
+
+// Wrist pulse width definitions in microseconds (us)
+#define WRIST_MIN 850
+#define WRIST_MAX 2350
+#define WRIST_DEFAULT 1600
+
+// Claw pulse width definitions in microseconds (us)
+#define CLAW_MIN 575
+#define CLAW_MAX 2460
+#define CLAW_DEFAULT 1518
 
 
 int main();
@@ -29,13 +48,37 @@ int main();
  */
 void initialize_PWM();
 
-/* Safely sets pulsewidth of shoulder turn servo
+/* Safely sets pulsewidth of shoulder servo
  * Registers effected:
  * OCR1A
  * Inputs:
  * width - pulse width in microseconds
  */
 void set_shoulder(uint16_t width);
+
+/* Safely sets pulsewidth of elbow servo
+ * Registers effected:
+ *
+ * Inputs:
+ * width - pulse width in microseconds
+ */
+void set_elbow(uint16_t width);
+
+/* Safely sets pulsewidth of wrist servo
+ * Registers effected:
+ *
+ * Inputs:
+ * width - pulse width in microseconds
+ */
+void set_wrist(uint8_t width);
+
+/* Safely set pulsewidth of claw servo
+ * Registers effected:
+ *
+ * Inputs:
+ * width - pulse width in microseconds
+ */
+void set_claw(uint8_t width);
 
 
 #endif
